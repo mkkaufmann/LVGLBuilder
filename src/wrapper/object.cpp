4f0146243959299d479b7f163112ef0c0b4eb7db
theol0403 <theol0403@gmail.com>
2019-11-05 23:23:43 -0800

    implement overloads

--- src/wrapper/object.cpp
+++ src/wrapper/object.cpp
@@ -1,91 +1,163 @@
 #include "wrapper/object.hpp"
 
-Object::Object() {
-  object = lv_obj_create(lv_scr_act(), NULL);
+Object::Object(lv_obj_t* iobject) : object(iobject) {}
+
+Object Object::create() {
+  return Object(lv_obj_create(lv_scr_act(), NULL));
 }
 
-Object::Object(Object* parent) {
-  object = lv_obj_create(parent->getPointer(), NULL);
+Object Object::create(const Object& iparent) {
+  return Object(lv_obj_create(iparent.get(), NULL));
 }
 
-Object::Object(Object* parent, Object* copy) {
-  object = lv_obj_create(parent->getPointer(), copy->getPointer());
+Object Object::create(const Object& iparent, const Object& icopy) {
+  return Object(lv_obj_create(iparent.get(), icopy.get()));
 }
 
 void Object::remove() {
   lv_obj_del(object);
 }
 
-Object& Object::clean() {
+Object& Object::clean() & {
   lv_obj_clean(object);
   return *this;
 }
 
-Object& Object::invalidate() {
+Object&& Object::clean() && {
+  lv_obj_clean(object);
+  return std::move(*this);
+}
+
+Object& Object::invalidate() & {
   lv_obj_invalidate(object);
   return *this;
 }
 
-Object& Object::setParent(Object* parent) {
-  lv_obj_set_parent(object, parent->getPointer());
+Object&& Object::invalidate() && {
+  lv_obj_invalidate(object);
+  return std::move(*this);
+}
+
+Object& Object::setParent(const Object& iparent) & {
+  lv_obj_set_parent(object, iparent.get());
   return *this;
 }
 
-Object& Object::setPosition(int x, int y) {
+Object&& Object::setParent(const Object& iparent) && {
+  lv_obj_set_parent(object, iparent.get());
+  return std::move(*this);
+}
+
+Object& Object::setPosition(int x, int y) & {
   lv_obj_set_pos(object, x, y);
   return *this;
 }
 
-Object& Object::setX(int x) {
+Object&& Object::setPosition(int x, int y) && {
+  lv_obj_set_pos(object, x, y);
+  return std::move(*this);
+}
+
+Object& Object::setX(int x) & {
   lv_obj_set_x(object, x);
   return *this;
 }
 
-Object& Object::setY(int y) {
+Object&& Object::setX(int x) && {
+  lv_obj_set_x(object, x);
+  return std::move(*this);
+}
+
+Object& Object::setY(int y) & {
   lv_obj_set_x(object, y);
   return *this;
 }
 
-Object& Object::setSize(int width, int height) {
+Object&& Object::setY(int y) && {
+  lv_obj_set_x(object, y);
+  return std::move(*this);
+}
+
+Object& Object::setSize(int width, int height) & {
   lv_obj_set_size(object, width, height);
   return *this;
 }
 
-Object& Object::setWidth(int width) {
+Object&& Object::setSize(int width, int height) && {
+  lv_obj_set_size(object, width, height);
+  return std::move(*this);
+}
+
+Object& Object::setWidth(int width) & {
   lv_obj_set_width(object, width);
   return *this;
 }
 
-Object& Object::setHeight(int height) {
+Object&& Object::setWidth(int width) && {
+  lv_obj_set_width(object, width);
+  return std::move(*this);
+}
+
+Object& Object::setHeight(int height) & {
   lv_obj_set_height(object, height);
   return *this;
 }
 
-Object& Object::align(Object* base, lv_align_t alignment, int xShift, int yShift) {
-  lv_obj_align(object, base->getPointer(), alignment, xShift, yShift);
+Object&& Object::setHeight(int height) && {
+  lv_obj_set_height(object, height);
+  return std::move(*this);
+}
+
+Object& Object::align(const Object& base, lv_align_t alignment, int xShift, int yShift) & {
+  lv_obj_align(object, base.get(), alignment, xShift, yShift);
   return *this;
 }
 
-Object& Object::alignToParent(lv_align_t alignment, int xShift, int yShift) {
+Object&& Object::align(const Object& base, lv_align_t alignment, int xShift, int yShift) && {
+  lv_obj_align(object, base.get(), alignment, xShift, yShift);
+  return std::move(*this);
+}
+
+Object& Object::alignToParent(lv_align_t alignment, int xShift, int yShift) & {
   lv_obj_align(object, lv_obj_get_parent(object), alignment, xShift, yShift);
   return *this;
 }
 
-Object& Object::alignOrigo(Object* base, lv_align_t alignment, int xShift, int yShift) {
-  lv_obj_align_origo(object, base->getPointer(), alignment, xShift, yShift);
+Object&& Object::alignToParent(lv_align_t alignment, int xShift, int yShift) && {
+  lv_obj_align(object, lv_obj_get_parent(object), alignment, xShift, yShift);
+  return std::move(*this);
+}
+
+Object& Object::alignOrigo(const Object& base, lv_align_t alignment, int xShift, int yShift) & {
+  lv_obj_align_origo(object, base.get(), alignment, xShift, yShift);
   return *this;
 }
 
-Object& Object::realign() {
+Object&& Object::alignOrigo(const Object& base, lv_align_t alignment, int xShift, int yShift) && {
+  lv_obj_align_origo(object, base.get(), alignment, xShift, yShift);
+  return std::move(*this);
+}
+
+Object& Object::realign() & {
   lv_obj_realign(object);
   return *this;
 }
 
-Object& Object::setAutoRealign(bool enabled) {
+Object&& Object::realign() && {
+  lv_obj_realign(object);
+  return std::move(*this);
+}
+
+Object& Object::setAutoRealign(bool enabled) & {
   lv_obj_set_auto_realign(object, enabled);
   return *this;
 };
 
-lv_obj_t* Object::getPointer() {
+Object&& Object::setAutoRealign(bool enabled) && {
+  lv_obj_set_auto_realign(object, enabled);
+  return std::move(*this);
+};
+
+lv_obj_t* Object::get() const {
   return object;
 }
