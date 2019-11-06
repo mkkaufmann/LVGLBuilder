b7a6634944f19b62237bd49bfdd36acc31d46c0d
theol0403 <theol0403@gmail.com>
2019-11-05 23:21:40 -0800

    update test

--- src/test.cpp
+++ src/test.cpp
@@ -6,9 +6,9 @@
 
 void ui_init() {
 
-  Object object1 = Object();
-  object1.setSize(100, 100).setPosition(50, 50);
-  Object obj2 = Object(&object1);
+  Object object1 = Object::create().setSize(100, 100).setPosition(50, 50);
+
+  Object obj2 = Object::create(object1);
   obj2.setSize(20, 20).setPosition(0, 0);
 
   while (true) {
