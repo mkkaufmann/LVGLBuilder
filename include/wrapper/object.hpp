981834ecac12f99f2c7cb43a5b870df72ee68754
theol0403 <theol0403@gmail.com>
2019-11-06 13:50:38 -0800

    format comments

--- include/wrapper/object.hpp
+++ include/wrapper/object.hpp
@@ -26,43 +26,55 @@ public:
   Object(Object&& iobject) = default;
   Object& operator=(Object&& iobject) = default;
 
-  /*
+  virtual ~Object() = default;
+
+  /**
    * Create object with no parameters. Defaults parent to lv_scr_act() and copy to NULL
    */
   static Object create();
 
-  /* Create object with parent parameter and defaults copy to NULL
+  /** 
+   * Create object with parent parameter and defaults copy to NULL
    *
    * @param iparent the parent object for this object to be a child of
    */
   static Object create(const Object& iparent);
 
-  /* Create object with both parent and copy parameters
+  /** 
+   * Create object with both parent and copy parameters
    *
    * @param iparent the parent object for this object to be a child of
    * @param icopy   the object for this object to copy
    */
   static Object create(const Object& iparent, const Object& icopy);
 
-  /* Delete this Object and all of its children */
+  /** 
+   * Delete this Object and all of its children
+   */
   void remove();
 
-  /* Delete all of this Object's children */
+  /** 
+   * Delete all of this Object's children
+   */
   Object& clean() &;
   Object&& clean() &&;
 
-  /* Mark the object as invalid (see LVGL docs) */
+  /** 
+   * Mark the object as invalid (see LVGL docs)
+   */
   Object& invalidate() &;
   Object&& invalidate() &&;
 
-  /* Set the parent of this Object
+  /** 
+   * Set the parent of this Object
    *
-   * @param parent the parent for this object to become a child of
+   * @param iparent the parent for this object to become a child of
    */
   Object& setParent(const Object& iparent) &;
   Object&& setParent(const Object& iparent) &&;
 
-  /* Set the position of the object
+  /** 
+   * Set the position of the object
    *
    * @param x the new x position of the object
    * @param y the new y position of the object
@@ -70,21 +82,24 @@ public:
   Object& setPosition(int x, int y) &;
   Object&& setPosition(int x, int y) &&;
 
-  /* Set the x position of the object
+  /** 
+   * Set the x position of the object
    *
    * @param x the new x coordinate (measured from left side of the parent)
    */
   Object& setX(int x) &;
   Object&& setX(int x) &&;
 
-  /* Set the y position of the object
+  /** 
+   * Set the y position of the object
    *
    * @param y the new y coordinate (measured from top of the parent)
    */
   Object& setY(int y) &;
   Object&& setY(int y) &&;
 
-  /* Set the size of the object
+  /** 
+   * Set the size of the object
    *
    * @param width  the new width of the object
    * @param height the new height of the object
@@ -92,14 +107,16 @@ public:
   Object& setSize(int width, int height) &;
   Object&& setSize(int width, int height) &&;
 
-  /* Set the width of the object
+  /** 
+   * Set the width of the object
    *
    * @param width the new width of the object
    */
   Object& setWidth(int width) &;
   Object&& setWidth(int width) &&;
 
-  /* Set the height of the object
+  /** 
+   * Set the height of the object
    *
    * @param height the new height of the object
    */
@@ -117,7 +134,8 @@ public:
   Object& align(const Object& base, lv_align_t alignment, int xShift, int yShift) &;
   Object&& align(const Object& base, lv_align_t alignment, int xShift, int yShift) &&;
 
-  /* Align the object to its parent
+  /** 
+   * Align the object to its parent
    *
    * @param alignment the type of alignment
    * @param xShift    the x-coordinate shift after alignment
@@ -126,7 +144,8 @@ public:
   Object& alignToParent(lv_align_t alignment, int xShift, int yShift) &;
   Object&& alignToParent(lv_align_t alignment, int xShift, int yShift) &&;
 
-  /* Align the object to another object
+  /** 
+   * Align the object to another object
    *
    * @param base      the object to align to
    * @param alignment the type of alignment
@@ -136,11 +155,14 @@ public:
   Object& alignOrigo(const Object& base, lv_align_t alignment, int xShift, int yShift) &;
   Object&& alignOrigo(const Object& base, lv_align_t alignment, int xShift, int yShift) &&;
 
-  /* Realign the object based on the last alignment */
+  /** 
+   * Realign the object based on the last alignment
+   */
   Object& realign() &;
   Object&& realign() &&;
 
-  /* Enable automatic realignment on resize
+  /** 
+   * Enable automatic realignment on resize
    *
    * @param enabled whether or not realignment is enabled
    */
