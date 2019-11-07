9bfbdc9454738231ff526565bfe55532430dbb18
theol0403 <theol0403@gmail.com>
2019-11-06 20:15:23 -0800

    format button

--- src/wrapper/button.cpp
+++ src/wrapper/button.cpp
@@ -12,22 +12,22 @@ Button Button::create(const Object& iparent, const Button& icopy) {
   return Button(lv_btn_create(iparent.get(), icopy.get()));
 }
 
-Button& Button::setToggle(bool enabled){
-		lv_btn_set_toggle(object, enabled);
-		return *this;
+Button& Button::setToggle(bool enabled) {
+  lv_btn_set_toggle(object, enabled);
+  return *this;
 }
 
- Button& Button::setState(lv_btn_state_t state){
-		lv_btn_set_state(object, state);
-		return *this;
+Button& Button::setState(lv_btn_state_t state) {
+  lv_btn_set_state(object, state);
+  return *this;
 }
 
-Button& Button::toggle(){
-		lv_btn_toggle(object);
-		return *this;
+Button& Button::toggle() {
+  lv_btn_toggle(object);
+  return *this;
 }
 
-Button& Button::setLayout(lv_layout_t layout){
-		lv_btn_set_layout(object, layout);
-		return *this;
+Button& Button::setLayout(lv_layout_t layout) {
+  lv_btn_set_layout(object, layout);
+  return *this;
 }
