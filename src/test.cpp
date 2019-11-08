ac4e2dd9e9fb6f2f88f3c3488df732f89b984bff
mkkaufmann <mkaufmann421@gmail.com>
2019-11-08 09:19:59 -0500

    Draw example ui

--- src/test.cpp
+++ src/test.cpp
@@ -8,20 +8,31 @@
 #include "wrapper/tileview.hpp"
 
 void ui_init() {
+  
+  //max height: 240
+  //max width: 480
+  const int screenHeight = 240;
+  const int screenWidth = 480;
+  
+  Object container = Object::create().setPosition(0, 0).setSize(screenWidth,screenHeight);
+  
+  Object field = Object::create(container).setPosition(0, 0).setSize(screenWidth, screenWidth);
 
-  Object obj1 = Object::create().setSize(100, 100).setPosition(50, 50);
+  for(int i = 0; i < 6; i++){
+		  for(int j = 0; j < 6; j++){
+				  Object tile = Object::create(field).setPosition(i * 40, j * 40).setSize(40, 40);
+		  }
+  }
 
-  Object obj2 = Object::create(obj1);
-  obj2.setSize(20, 20).setPosition(0, 0);
+  Object mainScreen = Object::create(container).setPosition(screenHeight, 0)
+		  .setSize(screenWidth-screenHeight-80, screenHeight);
 
-  Button btn = Button::create().setSize(200, 100).setPosition(0, 0);
-  static lv_style_t style_1;
-  lv_style_copy(&style_1, &lv_style_plain);
-  style_1.body.main_color = LV_COLOR_RED;
-  btn.setStyle(LV_BTN_STYLE_PR, &style_1);
+  Object sidebar = Object::create(container).setPosition(screenWidth - 80, 0).setSize(80, screenHeight);
 
-  ButtonMatrix btnm = ButtonMatrix::create();
-  TileView tileview = TileView::create();
+  for(int i = 0; i < 4; i++){
+		  Object button = Button::create(sidebar).setPosition(0, i * 60).setSize(80, 60);
+  }
+
   while (true) {
     usleep(1000000);
   }
