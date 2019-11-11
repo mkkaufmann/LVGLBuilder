b14356044f96b8c46cfa6e81e8eb92ae3413c411
acetousk <acetousk@gmail.com>
2019-11-11 08:46:07 -0700

    :bug: why does this not work

--- src/test.cpp
+++ src/test.cpp
@@ -7,6 +7,14 @@
 #include "wrapper/buttonmatrix.hpp"
 #include "wrapper/tileview.hpp"
 #include "wrapper/label.hpp"
+
+static lv_res_t click_action()
+{
+  printf("Clicked!\n");
+  return LV_RES_OK; /*Return OK because the button matrix is not deleted*/
+}
+
+
 void ui_init() {
   
   //max height: 240
@@ -24,7 +32,6 @@ void ui_init() {
 		  .setSize(screenWidth,screenHeight)
 		  .setStyle(&container_style);
   
-
   static lv_style_t field_style;
   lv_style_copy(&field_style, &lv_style_plain);
   field_style.body.main_color = LV_COLOR_WHITE;
@@ -63,13 +70,26 @@ void ui_init() {
 		  .setSize(screenWidth-screenHeight, screenHeight).setStyle(&main_style);
 
   Label mainContent = Label::create(mainScreen).setText(" This UI was made using\n LVGLBuilder, a\n collaboration between\n teams 4911A, 7842F and 914M.\n The library aims to ease\n the creation of UIs for\n programming beginners\n and veterans alike. The\n library is currently in\n early development.\0"); 
-/*
+//*
   Object sidebar = Object::create(container).setPosition(screenWidth - 80, 0).setSize(80, screenHeight);
 
   for(int i = 0; i < 4; i++){
 		  Button::create(sidebar).setPosition(0, i * 60).setSize(80, 60);
   }
-*/
+
+  lv_style_t * main_btn_style = &main_style;
+
+  Button button = Button::create()
+    .setAction(LV_BTN_ACTION_CLICK, click_action)
+    .setLayout( LV_LAYOUT_OFF )
+    .setFit( 100, 100 )
+    .toggle()
+    .setInkInTime(500)
+    .setInkOutTime(500)
+    .setInkWaitTime(500)
+    .setStyle(LV_BTN_STYLE_REL, main_btn_style);
+
+//*/
   while (true) {
     usleep(1000000);
   }
