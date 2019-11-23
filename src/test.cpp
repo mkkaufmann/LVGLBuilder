7e743bc9e016192339eeb496b8cc0936b45e7c43
Theo Lemay <theol0403@gmail.com>
2019-11-22 21:33:59 -0800

    Clean up test

--- src/test.cpp
+++ src/test.cpp
@@ -1,4 +1,3 @@
-
 #include "lvgl/lvgl.h"
 #include <iostream>
 #include <stdlib.h>
@@ -19,25 +18,25 @@ void test() {
   container.setSize(LV_HOR_RES, LV_VER_RES);
 
   ButtonMatrix btnm = ButtonMatrix::create(container);
-  btnm.setSize(LV_HOR_RES, LV_VER_RES / 4);
+  btnm.setSize(LV_HOR_RES, LV_VER_RES / 4.0);
 
   Label label = Label::create(container);
-  label.setSize(LV_HOR_RES / 4, LV_VER_RES * .75);
-  label.setPosition(0, LV_VER_RES / 4);
+  label.setSize(LV_HOR_RES / 4.0, LV_VER_RES * 0.75);
+  label.setPosition(0, LV_VER_RES / 4.0);
   label.setStaticText("    l\n    a\n    b\n    e\n    l\n\n  here  ");
 
-  lv_point_t points[] = {{LV_HOR_RES / 8, LV_VER_RES / 4}, {LV_HOR_RES / 8, LV_VER_RES}};
+  lv_point_t points[] = {{LV_HOR_RES / 8.0, LV_VER_RES / 4.0}, {LV_HOR_RES / 8.0, LV_VER_RES}};
   Line line = Line::create(container);
   line.setPoints(points, 2);
 
   Button button = Button::create(container);
-  button.setSize(LV_HOR_RES / 4, LV_VER_RES * .75);
-  button.setPosition(LV_HOR_RES / 8, LV_VER_RES / 4);
+  button.setSize(LV_HOR_RES / 4.0, LV_VER_RES * 0.75);
+  button.setPosition(LV_HOR_RES / 8.0, LV_VER_RES / 4.0);
   button.setText("Button");
 
   TileView tileView = TileView::create(container);
-  tileView.setSize(LV_HOR_RES * .625, LV_VER_RES * .75);
-  tileView.setPosition(LV_HOR_RES * .375, LV_VER_RES / 4);
+  tileView.setSize(LV_HOR_RES * 0.625, LV_VER_RES * 0.75);
+  tileView.setPosition(LV_HOR_RES * 0.375, LV_VER_RES / 4.0);
 }
 
 void ui_init() {
